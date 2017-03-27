842bb2fff9033802e50e35b80b7750bea33fe22c
Trejkaz (pen name) <trejkaz@trypticon.org>
2017-03-27 12:17:19 +1100

    Line to set the filter not to delete rendered messages, present in production code as well.

--- src/main/java/com/example/WicketApplication.java
+++ src/main/java/com/example/WicketApplication.java
@@ -3,7 +3,17 @@ package com.example;
 import org.apache.wicket.protocol.http.WebApplication;
 
 public class WicketApplication extends WebApplication
-{    	
+{
+	@Override
+	protected void init()
+	{
+		super.init();
+
+		// Only delete messages which have actually been rendered.
+		// This workaround used to be in an overridden session class, but moved to here as part of the wicket 6 changes.
+		getApplicationSettings().setFeedbackMessageCleanupFilter(message -> message.getReporter() != null && message.isRendered());
+	}
+
 	@Override
 	public Class<HomePage> getHomePage()
 	{
